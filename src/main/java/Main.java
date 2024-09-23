c9e114d2d54203ff00e96c0fcdab5da5f04c837b
Ali Traore <tr.ali70404@gmail.com>
2024-09-23 15:31:39 +0200

    Affichage ali

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         DBConfig.LoadDBConfig("src/main/json/file-config.json");
         System.out.println("Hello World");
+        System.out.println("ali");
     }
 }
