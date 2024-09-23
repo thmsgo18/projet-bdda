6657cbc529c516ebed3ab113751247da83ce7d24
GloglopTagada <134527237+GloglopTagada@users.noreply.github.com>
2024-09-23 14:47:54 +0200

    projet v1

---
+++ src/main/java/DBConfig.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.*;
+
+public class DBConfig{
+
+    private String dbpath; //chemin d'accès sous forme de string
+
+    public DBConfig(String dbpath){ //Constructeur
+        this.dbpath = dbpath;
+    }
+
+    public static DBConfig LoadDBConfig(String fichier_config){ //méthode qui va lire fichier et le transformé en Json pour acceder à la valeur de la clé dbpath
+        try{
+            FileReader fr = new  FileReader(fichier_config);//Utilisation des classes FileReader et BufferReader pour lire le fichier
+            BufferedReader bfr = new BufferedReader(fr);
+            StringBuilder sb =new StringBuilder();
+            String ligne ;
+            while((ligne = bfr.readLine())!=null){ //line vaut la ligne du fichier(ex: si fichier contient une ligne : "dbpath : ././DB" alors line vaut dbpath. boucle continue jusqu'a plus de ligne.
+                sb.append(ligne); // on ajoute la ligne au StringBuffer car StringBuffer est plus flexible d'utilisation.
+            }
+            bfr.close();//Fermeture de la lecture du fichier
+            JSONObject js = new JSONObject(sb.toString());//Creer une instance de JsonObject pour recuperer la ligne qui sera transformer en Json
+            return new DBConfig(js.getString("dbpath"));//recupere la valeur de la clé dbpath et retourne une nouvelle instance de DBConfig.
+
+
+        }catch(IOException io){ //Si fichier marche pas donc retourner nul
+            System.out.println("Nullllllll");
+            io.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public String getDbpath(){ //getteur
+        return dbpath;
+    }
+    public void setDbpath(String dbpath){//setteur
+        this.dbpath = dbpath;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        DBConfig.LoadDBConfig("src/main/json/file-config.json");
+    }
+}
