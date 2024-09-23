732a22df4d5abe4bae28e4f7cde4c0634c4a9089
Ali Traore <tr.ali70404@gmail.com>
2024-09-23 15:27:01 +0200

    Affichage Hello Word

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         DBConfig.LoadDBConfig("src/main/json/file-config.json");
+        System.out.println("Hello World");
     }
 }
