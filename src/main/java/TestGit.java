9056e8aefb4db51e4af00cb7d7861326a60db502
malik439 <Mlkdf.7518@gmail.com>
2024-09-23 15:48:50 +0200

    Ajout d'une classe TestGit et de sa méthode afficher

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,10 @@
+public class TestGit {
+
+   public static void afficher(){
+       System.out.println("Membre du groupe :\n");
+       System.out.println("Ali");
+       System.out.println("Valentin");
+       System.out.println("Thomas");
+       System.out.println("Malik");
+   }
+}
