8e50f92161f44819671d5d70d8255c83302f1649
malik439 <Mlkdf.7518@gmail.com>
2024-09-26 16:28:08 +0200

    Création classe DiskManagerTests

---
+++ src/main/java/DiskManagerTests.java
@@ -0,0 +1,33 @@
+
+public class DiskManagerTests {
+    public static void main(String[] args) {
+
+        DBConfig dbc = new DBConfig("",0,0);
+        DiskManager dm = new DiskManager(dbc);
+
+    }
+
+    public void TestAllocPage(DiskManager dm){
+
+    }
+
+    public void TestDeallocPage(DiskManager dm){
+
+    }
+
+    public void TestEcriturePage(DiskManager dm){
+
+    }
+
+    public void TestLecturePage(DiskManager dm){
+
+    }
+
+    public void TestSaveState(DiskManager dm){
+
+    }
+
+    public void TestLoadState(DiskManager dm){
+
+    }
+}
