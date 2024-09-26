fc54e99e85d81c807666855c3292135a02275102
GloglopTagada <134527237+GloglopTagada@users.noreply.github.com>
2024-09-26 16:24:13 +0200

    Modif main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         DBConfig.LoadDBConfig("src/main/json/file-config.json");
-        System.out.println("Hello World");
-        System.out.println("ali");
-        System.out.println("Malik");
-        System.out.println("Hala Madrid");
-        System.out.println("Mès que un club");
     }
 }
