a5f2078df9b3a4a95016835cc073458c11385b70
malik439 <Mlkdf.7518@gmail.com>
2024-10-10 17:09:30 +0200

    Squelette classe Record

--- src/main/java/ColInfo.java
+++ src/main/java/ColInfo.java
@@ -1,4 +1,22 @@
 public class ColInfo {
     private String nomColonne;
     private String typeColonne;
+
+    public ColInfo(String nomColonne, String typeColonne) {
+        this.nomColonne = nomColonne;
+        this.typeColonne = typeColonne;
+    }
+
+    public ColInfo(String nomColonne) {
+        this.nomColonne = nomColonne;
+        this.typeColonne = "";
+    }
+
+    public String getNomColonne() {
+        return nomColonne;
+    }
+
+    public String getTypeColonne() {
+        return typeColonne;
+    }
 }
