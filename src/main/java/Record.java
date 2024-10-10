96dd8e82dda58e00b47732be402921f6506e8d3b
malik439 <Mlkdf.7518@gmail.com>
2024-10-10 16:32:53 +0200

    Squelette classe Record

--- src/main/java/Record.java
+++ src/main/java/Record.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Record {
+
+    List<Object> valeurs;
+
+    public Record(List<Object> valeurs) {
+        this.valeurs = valeurs;
+    }
+
+    public Record(){
+        valeurs = new ArrayList<Object>();
+    }
+
+    public Object getValeur(int i) {
+        return valeurs.get(i);
+    }
+
+    public void setValeur(int i, Object valeur) {
+        valeurs.set(i, valeur);
+    }
+
+    public String toString(){
+        StringBuilder retour = new StringBuilder();
+        retour.append("[");
+        for (Object valeur : valeurs) {
+            retour.append(valeur);
+            retour.append(",");
+        }
+        retour.append("]");
+        return retour.toString();
+    }
 }
