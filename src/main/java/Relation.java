425565a761b9d8a370d825debac012842bb44a15
Yvngval <158499842+Yvngval@users.noreply.github.com>
2024-10-10 15:56:39 +0200

    squelette de la classe Relation

--- src/main/java/Relation.java
+++ src/main/java/Relation.java
@@ -1,7 +1,34 @@
+import java.nio.ByteBuffer;
+
 public class Relation {
 
     private String nomRelation;
     private int nbColonnes;
+     // Ajouter comme attribut CallInfo.
+    public Relation(String nomRelation, int nbColonnes) {
+        this.nomRelation = nomRelation;
+        this.nbColonnes = nbColonnes;
+    }
+
+    public String getNomRelation() {
+        return nomRelation;
+    }
+
+    public int getNbColonnes() {
+        return nbColonnes;
+    }
+
+    public int writeRecordtoBuffer(Record r, ByteBuffer buff,int pos) {
+
+
+        return 0;
+    }
+
+    public int readFromBuffer(ByteBuffer buff, int pos) {
+
+
+        return 0;
+    }
 
 
 }
