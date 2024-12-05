e193e89982b1600ff64ff84e3b36aeb8e2099733
malik439 <Mlkdf.7518@gmail.com>
2024-12-05 20:32:07 +0100

    implementation SelectOperator

--- src/main/java/SelectOperator.java
+++ src/main/java/SelectOperator.java
@@ -1,15 +1,35 @@
 
+
 public class SelectOperator implements IRecordIterator<Record> {
 
     private IRecordIterator<Record> operateurFils;
+    //private Condition condition; le temps que Ali finit la classe condition
+    private boolean condition;
+
+    public SelectOperator(IRecordIterator<Record> operateurFils, Boolean condition) {
+        this.operateurFils = operateurFils;
+        this.condition = condition;
+
+    }
+
+    //Vérifie si le record venant de l'operateur fils remplit les conditions "WHERE" pour le return
 
     public Record GetNextRecord(){
-        return operateurFils.GetNextRecord();
+        Record record = new Record();
+
+        while((record = operateurFils.GetNextRecord()) != null){
+            if(condition){
+                return record;
+            }
+        }
+        return null;
     }
+
 
     public void Close(){
         operateurFils.Close();
     }
+
 
     public void Reset(){
         operateurFils.Reset();
